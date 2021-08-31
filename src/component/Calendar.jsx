3fe91266b3ae3875691116385f9a1e07160d7aab
whywesmurfing <raymond.doan94@gmail.com>
2021-08-31 20:56:37 +1000

    Added fullcalendar plugins and added calendar to App

---
+++ src/component/Calendar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+const Calendar = () => {
+  return (
+    <FullCalendar
+      plugins={[ dayGridPlugin ]}
+      initialView="dayGridMonth"
+    />
+  )
+}
+
+export default Calendar
